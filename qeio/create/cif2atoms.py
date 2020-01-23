51c66ab8ac869929e5a43e528ac180a8af24c017
iorin0217 <ikuratan217@gmail.com>
2020-01-24 03:44:57 +0900

    pw完成

--- qeio/create/cif2atoms.py
+++ qeio/create/cif2atoms.py
@@ -4,7 +4,7 @@ from pymatgen.core.periodic_table import get_el_sp
 import seekpath
 import json
 
-def create_atoms(cif, supercell_matrix=np.eye(3), spins, press, symm):
+def create_atoms(cif,trans=np.eye(3), spins, press, symm):
     atoms = {}
     structure = pymatgen.Structure.from_file(cif)
     # cif ioだと変わる
@@ -16,32 +16,37 @@ def create_atoms(cif, supercell_matrix=np.eye(3), spins, press, symm):
     atoms["bvec"] = skp["reciprocal_primitive_lattice"]
     atoms["pos"] = skp["primitive_positions"]
     atoms["nk"] = np.round(np.linalg.norm(bvec, axis=0)/ 0.2)
+    for ipath in range(len(skp["path"])):
+        start = skp["explicit_segments"][ipath][0]
+        final = skp["explicit_segments"][ipath][1] - 1
+    print("%5d %8s %10.5f %10.5f %10.5f %8s %10.5f %10.5f %10.5f" % (
+        final - start + 1,
+        skp["explicit_kpoints_labels"][start],
+        skp["explicit_kpoints_rel"][start][0],
+        skp["explicit_kpoints_rel"][start][1],
+        skp["explicit_kpoints_rel"][start][2],
+        skp["explicit_kpoints_labels"][final],
+        skp["explicit_kpoints_rel"][final][0],
+        skp["explicit_kpoints_rel"][final][1],
+        skp["explicit_kpoints_rel"][final][2]))
+    #nks
+    #kpath
+    print(len(skp["explicit_kpoints_rel"]), file=f)
+    for ik in range(len(skp["explicit_kpoints_rel"])):
+        print(" %f %f %f 1.0" % (
+            skp["explicit_kpoints_rel"][ik][0],
+            skp["explicit_kpoints_rel"][ik][1],
+            skp["explicit_kpoints_rel"][ik][2]),
+            file=f)
     #ibrav
-    #angle
+    #trans
     #press
     atoms["atom"] = [str(get_el_sp(iat)) for iat in skp["primitive_types"]]
     with open("../../settings/elements.json", "r") as f:
         elements = json.load(f)
     atoms["nat"] = len(skp["primitive_types"])
+    #psdir/psfile → nwfc
     #soc, U
     #typ(mag) → ntyp
-    #psdir/psfile → nwfc
 
     return atoms
-    #
-    # Band path
-    #
-    print("Band path")
-    for ipath in range(len(skp["path"])):
-        start = skp["explicit_segments"][ipath][0]
-        final = skp["explicit_segments"][ipath][1] - 1
-        print("%5d %8s %10.5f %10.5f %10.5f %8s %10.5f %10.5f %10.5f" % (
-            final - start + 1,
-            skp["explicit_kpoints_labels"][start],
-            skp["explicit_kpoints_rel"][start][0],
-            skp["explicit_kpoints_rel"][start][1],
-            skp["explicit_kpoints_rel"][start][2],
-            skp["explicit_kpoints_labels"][final],
-            skp["explicit_kpoints_rel"][final][0],
-            skp["explicit_kpoints_rel"][final][1],
-            skp["explicit_kpoints_rel"][final][2]))
--- qeio/create/pw.py
+++ qeio/create/pw.py
@@ -3,13 +3,19 @@
 def create_pw_in(path, atoms, calculation="scf", restart_mode="from_scratch"):
     control_noend = ["&CONTROL", f"calculation = '{calculation}'", f"restart_mode = '{restart_mode}'", f"pseudo_dir = '{atoms["psdir"]}'", "/"]
     systems_noend = ["&SYSTEM", "ibrav = 0", f"nat = {atoms['nat']}", f"ntyp = {atoms['ntyp']}", f"ecutwfc = {atoms['ecutwfc']}", f"ecutrho = {atoms['ecutrho']}", "occupations = 'tetrahedra_opt'"]
-    atomic_parameters = []
-    # spin
-    # hubbard
+    spin = []
+    if atoms['nspin'] == 2:
+        spin = ["nspin = 2"] + [f"starting_magnetization({i}) = {atoms['starting_magnetization'][i]}" for i in atoms['starting_magnetization']]
+    elif atoms['nspin'] == 4:
+        spin = ["noncolin = .true.", "lspinorb = .true."] + [f"starting_magnetization({i}) = {atoms['starting_magnetization'][i]}, angle1({i}) = {atoms['angle1'][i]}, angle2({i}) = {atoms['angle2'][i]}" for i in atoms['starting_magnetization']]
+    hubbard = []
+    if atoms['lda_plus_u'] == True:
+        hubbard = ["lda_plus_u = .true."] + [f"Hubbard_U({i}) = {atoms['Hubbard_U'][i]}" for i in atoms['Hubbard_U']]
+    atomic_parameters = spin + hubbard
     electrons = ["&ELECTRONS", f"conv_thr = {float(atoms["nat"])*1.0e-10}", "mixing_beta = 0.2", "diagonalization = 'david'", "/"]
-    cell_parameters = [" %f %f %f" % (i[0],i[1],i[2]) for i in avec]
-    atomic_species = [" %s %f %s" % (ityp, pymatgen.Element(ityp).atomic_mass, oncv[str(ityp)]["filename"]) for ityp in typ]
-    atomic_positions = [" %s %f %f %f" % (atom[iat], pos[iat][0], pos[iat][1], pos[iat][2]) for iat in range(nat)]
+    cell_parameters = [f"{vec[0]} {vec[1]} {vec[2]}" for vec in avec]
+    atomic_species = [f"{ityp} {pymatgen.Element(ityp).atomic_mass} {oncv[str(ityp)]["filename"]}" for ityp in typ]
+    atomic_positions = [f"{atom[iat]} {pos[iat][0]} {pos[iat][1]} {pos[iat][2]}" for iat in range(nat)]
     if calculation == "scf":
         kpoints = ["K_POINTS automatic", f"{int(nk[0])} {int(nk[1])} {int(nk[2])} 0 0 0"]
         scf_in = control_noend + ["/"] + systems_noend + atomic_parameters + ["/"] + electrons + ["CELL_PARAMETERS angstrom"] + cell_parameters + ["ATOMIC_SPECIES"] + atomic_species + ["ATOMIC_POSITIONS crystal"] + atomic_positions + kpoints
@@ -25,7 +31,7 @@ def create_pw_in(path, atoms, calculation="scf", restart_mode="from_scratch"):
         nscf_in = control_noend + ["/"] + systems_noend + atomic_parameters + nbnd + electrons + ["CELL_PARAMETERS angstrom"] + cell_parameters + ["ATOMIC_SPECIES"] + atomic_species + ["ATOMIC_POSITIONS crystal"] + atomic_positions + kpoints
     elif calculation == "bands":
         nbnd = [f"nbnd = {atoms['nwfc']}", "/"]
-        #kpath
+        kpath = ["K_POINTS crystal"] + [f"{atoms['nks']}"] + [f"{kcoord[0]} {kcoord[1]} {kcoord[2]} 1.0" for kcoord in atoms['kpath']]
         nscf_in = control_noend + ["/"] + systems_noend + atomic_parameters + nbnd + electrons + ["CELL_PARAMETERS angstrom"] + cell_parameters + ["ATOMIC_SPECIES"] + atomic_species + ["ATOMIC_POSITIONS crystal"] + atomic_positions + kpath
     print(eval(f"*{calculation}_in, sep='\n', end='\n', file=open(f'{path}/{calculation}.in','w')"))
 
