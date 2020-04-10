5bb082bc68f36ce7de8d839ce65c2af530e1399b
iorin0217 <ikuratan217@gmail.com>
2020-04-11 01:53:37 +0900

    fermi面の描画

--- plotsample.py
+++ plotsample.py
@@ -1,16 +1,30 @@
 # %%
 import plotly.offline as pltly
-import plotly.figure_factory as ff
 import plotly.graph_objs as go
-import re
 import numpy as np
-from scipy.spatial import Voronoi
-from pymatgen import Structure, Lattice
-from pymatgen.core.periodic_table import Element
+
 # %%
+with open("/home/CMD35/cmd35stud07/experiments/SRO_fine/vfermi.frmsf", "r") as f:
+    all_data = f.readlines()
+    frmsf = [i.rstrip().split() for i in all_data]
+nk = frmsf[0]
+points = nk[0]*nk[1]*nk[2]
+nbands = frmsf[2]
+bvec = frmsf[3:6]
 
-# plot
-pltly.iplot(go.Figure(data=data, layout=layout))
-
+# %%
+X, Y, Z = np.mgrid[0:nk[0]:16j, 0:nk[1]:16j, 0:nk[2]:16j]
+datas = []
+for i in range(nbands):
+    datas.append(go.Isosurface(
+        x=(X@np.array(bvec[0])).flatten(),
+        y=(Y@np.array(bvec[1])).flatten(),
+        z=(Z@np.array(bvec[2])).flatten(),
+        value=frmsf[6+points*i:6+points*(i+1)],
+        isomin=0,
+        isomax=0,
+        caps=dict(x_show=False, y_show=False)
+    ))
+pltly.iplot(go.Figure(data=datas))
 
 # %%
